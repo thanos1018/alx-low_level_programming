718a8411c05731cdcf5accc3ed58064d18c08832
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-16 12:29:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,19 @@
 #include <unistd.h>
-
 /**
- * main - Entry point of the program
- *
- * Return: Always 1 (Error)
- */
+* main - Entry point of the program
+*
+* Return: Always 1 (Error)
+*/
 int main(void)
 {
-    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int len = 0;
-
-    while (str[len] != '\0')
-    {
-        len++;
-    }
-
-    write(2, str, len);
-
-    return (1);
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+write(2, str, len);
+return (1);
 }
 
 
