c76b434060e08bfa2e79aa0d2da7438158fb0d4b
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-16 12:11:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,25 @@
 #include <unistd.h>
-#define STDERR_FILENO 2
 
-int main(void) {
-  char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  ssize_t length = sizeof(message) - 1;
-  if (write(STDERR_FILENO, message, length) != length) {
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    const size_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+    /* Write the message to the standard error */
+    ssize_t count = write(STDERR_FILENO, msg, len);
+
+    /* Check if the write operation was successful */
+    if (count != (ssize_t)len)
+    {
+        return 1;
+    }
+
     return 1;
-  }
-  return 1;
 }
 
 
