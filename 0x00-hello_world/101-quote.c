ad0d8ad694f145bf102f87d53088eb66dd971b1c
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-16 12:07:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1.
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
-	return (1);
+#define STDERR_FILENO 2
+
+int main(void) {
+  char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  ssize_t length = sizeof(message) - 1;
+  if (write(STDERR_FILENO, message, length) != length) {
+    return 1;
+  }
+  return 1;
 }
+
 
 
