e7dec8c1fdb3a588d9e630b7d1b9d758791a8a9b
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-16 12:22:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,18 +3,21 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints a message to the standard error
- * using the write function
- *
  * Return: Always 1 (Error)
  */
-
 int main(void)
 {
-    ssize_t len = sizeof("and that piece of art is useful" - 1);
+    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int len = 0;
 
-    /* write to the standard error */
-    write(STDERR_FILENO, "and that piece of art is useful\n", len);
+    /* Calculate the length of the string */
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    /* Write the string to the standard error */
+    write(2, str, len);
 
     return (1);
 }
