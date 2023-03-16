ef2133e12e5622cbc9f44852b8221dffed8acd33
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-16 12:25:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,13 +10,11 @@ int main(void)
     char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     int len = 0;
 
-    /* Calculate the length of the string */
     while (str[len] != '\0')
     {
         len++;
     }
 
-    /* Write the string to the standard error */
     write(2, str, len);
 
     return (1);
