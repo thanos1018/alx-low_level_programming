702f053537468f6a96ddac3b6b60cffab83c5a8a
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-17 14:58:55 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+* main - prints all possible combinations of single-digit numbers
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
+
