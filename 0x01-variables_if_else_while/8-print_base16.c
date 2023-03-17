ecaca0ec28a17e94a8826d9c01577615e4244444
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-17 14:45:07 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,17 +10,20 @@ int main(void)
 {
 int i;
 char c;
-for (i = 0; i < 10; i++) {
+for (i = 0; i < 10; i++)
+{
 c = i + '0';
 putchar(c);
 }
-for (i = 0; i < 6; i++) {
+for (i = 0; i < 6; i++)
+{
 c = i + 'a';
 putchar(c);
 }
 putchar('\n');
 return 0;
 }
+
 
 
 
