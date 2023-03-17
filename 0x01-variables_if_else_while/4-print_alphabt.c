8adeb213d5356ce9c88113f0dbbbfcb3260cdd25
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-17 10:49:39 -0700

    4-print_alphabet.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,18 @@
-/*
-* main - entry point
+#include <stdio.h>
+/**
+* main - Entry point
 *
-* Description: This program prints the alphabet in lowercase, followed by a new line. It then prints all the letters except 'q' and 'e', using the putchar function twice.
-*
-* Return: Always 0 (success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-char c;
-/* Print the alphabet in lowercase */
-for (c = 'a'; c <= 'z'; c++)
-putchar(c);
-/* Print all the letters except 'q' and 'e' */
-for (c = 'a'; c <= 'z'; c++)
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (c == 'e' || c == 'q')
-continue;
-putchar(c);
+if (letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
 }
 putchar('\n');
 return (0);
