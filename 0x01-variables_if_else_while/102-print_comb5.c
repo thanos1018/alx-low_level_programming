a882c96083880804c16c0f5e5b3ae8a7ff464988
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-18 05:08:49 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+/**
+* main - prints all possible combinations of two two-digit numbers
+* Return: 0 on success
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = i; j < 100; j++)
+{
+if (i != j)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i < 98 || j < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
