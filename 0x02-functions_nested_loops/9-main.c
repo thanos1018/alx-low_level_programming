c0e0e8ec6651a43237f3add2cf0f8eabd8830904
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-20 16:44:45 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,5 +1,33 @@
 #include "main.h"
 /**
+* times_table - prints the 9 times table
+*/
+void times_table(void)
+{
+int i, j, result;
+for (i = 0; i <= 9; i++) {
+for (j = 0; j <= 9; j++) {
+result = i * j;
+if (result > 9) {
+/* If result is 2 digit, print with space */
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+} else {
+/* If result is 1 digit, print with spaces */
+if (j != 0) {
+_putchar(' ');
+}
+_putchar(result + '0');
+}
+if (j != 9) {
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+/**
 * main - check the code
 *
 * Return: Always 0.
@@ -9,3 +37,4 @@ int main(void)
 times_table();
 return (0);
 }
+
