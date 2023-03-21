8b9c74e6491c2e590e64d47e64aac92f8246ecc2
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-20 17:02:12 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,41 +1,35 @@
 #include "main.h"
 /**
-* times_table - prints the 9 times table
+* times_table - prints the 9 times table, starting with 0
 */
 void times_table(void)
 {
-int i, j, result;
-for (i = 0; i <= 9; i++) {
-for (j = 0; j <= 9; j++) {
-result = i * j;
-if (result > 9) {
-/* If result is 2 digit, print with space */
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
-} else {
-/* If result is 1 digit, print with spaces */
-if (j != 0) {
-_putchar(' ');
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
 }
-_putchar(result + '0');
-}
-if (j != 9) {
+if (k < 10 && j != 0)
+{
 _putchar(',');
 _putchar(' ');
+_putchar(' ');.
+_putchar(k + '0');
+} else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
 }
-}
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-times_table();
-return (0);
 }
 
 
