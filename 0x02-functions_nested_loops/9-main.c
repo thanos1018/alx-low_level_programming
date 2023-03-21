10ebb6614a4c000e384e30c1935e07b67175773f
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-20 17:24:27 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -4,29 +4,28 @@
 */
 void times_table(void)
 {
-int i, j, k;
-for (i = 0; i < 10; i++)
+int i, j;
+for (i = 0; i <= 9; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; j <= 9; j++)
 {
-k = j * i;
+if ((i * j) < 10)
+{
 if (j == 0)
-{
-_putchar(k + '0');
+_putchar('0');
+else
+_putchar(' ');
+_putchar((i * j) + '0');
 }
-if (k < 10 && j != 0)
+else
+{
+_putchar((i * j) / 10 + '0');
+_putchar((i * j) % 10 + '0');
+}
+if (j != 9)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-} 
-else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
