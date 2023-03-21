08d524163775104ce848869cdf06da2bd9c02c35
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-20 18:31:30 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -1,30 +1,15 @@
 #include "main.h"
 /**
-* print_to_98 - prints all natural numbers from n to 98, followed by a new line
-* @n: the starting number
+* main - check the code
 *
-* Return: void
+* Return: Always 0.
 */
-void print_to_98(int n)
+int main(void)
 {
-int i;
-if (n <= 98)
-{
-for (i = n; i < 98; i++)
-{
-printf("%d, ", i);
+print_to_98(0);
+print_to_98(98);
+print_to_98(111);
+print_to_98(81);
+print_to_98(-10);
+return (0);
 }
-}
-else
-{
-for (i = n; i > 98; i--)
-{
-printf("%d, ", i);
-}
-}
-printf("98\n");
-}
-
-
-
-
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_to_98 - prints all natural numbers from n to 98,
-* followed by a new line
-* @n: print from this number
+* print_to_98 - prints all natural numbers from n to 98, followed by a new line
+* @n: the starting number
+*
+* Return: void
 */
 void print_to_98(int n)
 {
-int i, j;
+int i;
 if (n <= 98)
 {
-for (i = n; i <= 98; i++)
+for (i = n; i < 98; i++)
 {
-if (i != 98)
 printf("%d, ", i);
-else if (i == 98)
-printf("%d\n", i);
-} else if (n >= 98)
-{
-for (j = n; j >= 98; j--)
-{
-if (j != 98)
-printf("%d, ", j);
-else if (j == 98)
-printf("%d\n", j);
 }
 }
+else
+{
+for (i = n; i > 98; i--)
+{
+printf("%d, ", i);
+}
+}
+printf("98\n");
+}
+
