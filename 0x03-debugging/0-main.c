4952896328a00b1099f6cffbb71a9d70806aefeb
Thato-mphomane <tmphomane1018@gmail.com>
2023-03-23 13:32:39 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive pr negative
 * Return: 0
 */
 int main(void)
@@ -10,4 +10,3 @@ i = 0;
 positive_or_negative(i);
 return (0);
 }
-
